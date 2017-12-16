2419eaa5360687b6431fc905916b1aa9f302c7a2
RamiroCope <RamiroCope@github.com>
2017-12-16 16:21:23 +0100

    hyper net

--- test_area/reader.py
+++ test_area/reader.py
@@ -80,11 +80,12 @@ def ptb_raw_data(data_path=None):
   test_path = os.path.join(data_path, "ptb.test.txt")
 
   word_to_id = _build_vocab(train_path)
+  id_to_word = {v:k for k,v in word_to_id.items()}
   train_data = _file_to_word_ids(train_path, word_to_id)
   valid_data = _file_to_word_ids(valid_path, word_to_id)
   test_data = _file_to_word_ids(test_path, word_to_id)
   vocabulary = len(word_to_id)
-  return train_data, valid_data, test_data, vocabulary
+  return train_data, valid_data, test_data, vocabulary, id_to_word
 
 
 def ptb_producer(raw_data, batch_size, num_steps, name=None):
